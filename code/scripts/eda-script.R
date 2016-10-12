5fc0c603f86976d1e529f2ca11f733e6b39e06a5
nikkirethyo <nikkireth@berkeley.edu>
2016-10-12 00:32:44 -0700

    updated relative paths in eda-script.R

--- code/scripts/eda-script.R
+++ code/scripts/eda-script.R
@@ -1,23 +1,23 @@
 #read in data
-data <- read.csv("/Users/nrethmeier/Documents/stat159/stat159-fall2016-hw03/data/Advertising.csv")
+data <- read.csv("../../data/Advertising.csv")
 
 #histogram for sales
-png("/Users/nrethmeier/Documents/stat159/stat159-fall2016-hw03/images/histogram-sales.png")
+png("../../images/histogram-sales.png")
 hist(data$Sales, xlab="Sales", main="Histogram of Sales")
 dev.off()
 
 #histogram for tv
-png("/Users/nrethmeier/Documents/stat159/stat159-fall2016-hw03/images/histogram-tv.png")
+png("../../images/histogram-tv.png")
 hist(data$TV, xlab="TV", main="Histogram of TV")
 dev.off()
 
 #histogram for radio
-png("/Users/nrethmeier/Documents/stat159/stat159-fall2016-hw03/images/histogram-radio.png")
+png("../../images/histogram-radio.png")
 hist(data$Radio, xlab="Radio", main="Histogram of Radio")
 dev.off()
 
 #histogram for newspaper
-png("/Users/nrethmeier/Documents/stat159/stat159-fall2016-hw03/images/histogram-newspaper.png")
+png("../../images/histogram-newspaper.png")
 hist(data$Newspaper, xlab="Newspaper",main="Histogram of Newspaper")
 
 #summary statistics of sales 
@@ -38,7 +38,7 @@ mat[lower.tri(mat)]=""
 correlation <- data.frame(mat)
 
 #create txt file of summary statistics 
-sink("/Users/nrethmeier/Documents/stat159/stat159-fall2016-hw03/data/eda-output.txt")
+sink("../../data/eda-output.txt")
 print("Summary of Sales")
 print(sales.sum)
 print("Summary of TV")
@@ -52,9 +52,9 @@ print(correlation)
 sink()
 
 #save scatterplot matrix for variables 
-png("/Users/nrethmeier/Documents/stat159/stat159-fall2016-hw03/images/scatterplot-matrix.png")
+png("../../images/scatterplot-matrix.png")
 data <- data[,2:5]
 pairs(data, pch=16, cex=.5, main="Scatterplot Matrix of Variables")
 dev.off()
 #save binary data of correlations 
-save(correlation, file="/Users/nrethmeier/Documents/stat159/stat159-fall2016-hw03/data/correlation-matrix.Rdata") 
+save(correlation, file="../../data/correlation-matrix.Rdata") 
